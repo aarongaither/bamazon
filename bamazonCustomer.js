d149483502a6525b0b68ed8ee149d6103fe94136
Aaron Gaither <AaronGaither@coresystems.local>
2017-07-04 23:36:09 -0700

    change to mysql promise

---
+++ bamazonCustomer.js
@@ -0,0 +1,64 @@
+mysql = require('promise-mysql');
+inq = require('inquirer');
+
+const customerPrompt = () => {
+	let connection;
+	let productInfo;
+	mysql.createConnection({
+	  host     : 'localhost',
+	  user     : 'root',
+	  password : 'ucsd0417',
+	  database : 'bamazon'
+	}).then((conn) => {
+		connection = conn;
+		return connection.query('SELECT item_id, product_name, price, stock_quantity FROM products;')
+	}).then(rows => {
+	    rows.forEach(val => {console.log(`${val.item_id} - ${val.product_name} - ${val.price} - ${val.stock_quantity}`)});
+	    return rows
+	}).then(rows => {
+		return inq.prompt({
+			type: "input",
+			name: "productID",
+			message: "What would you like to purchase?"
+		})
+	}).then(res => {
+		return connection.query('SELECT item_id, product_name, price, stock_quantity FROM products WHERE item_id=?;', res.productID)
+	}).then(row => {
+		productInfo = row[0];
+		return inq.prompt({
+			type: "input",
+			name: "qty",
+			message: `${productInfo.product_name} - How many would you like?`
+		})
+	}).then(res => {
+		let qty = res.qty;
+		if (qty > productInfo.stock_quantity) {
+			console.log('Sorry, there is not enough remaining in stock to fulfill your order!')
+		} else {
+			updateInventory(productInfo.item_id, qty);
+			console.log('Your purchase info:')
+			console.log(`${productInfo.product_name}, ${qty}, ${productInfo.price}, ${qty * productInfo.price}.`)
+		}
+	}).then(() => {
+		return inq.prompt({
+			type: "confirm",
+			name: "repeat",
+			message: "Would you like to make another purchase?"
+		})
+	}).then((res) => {
+		if (res.repeat) {
+			console.log('Con end from cst prompt, before repeat.')
+			connection.end();
+			customerPrompt();
+		} else {
+			console.log('Con end from cst prompt.')
+			connection.end();
+		}
+	});
+}
+
+const updateInventory = (id, qty) => {
+	console.log('Inventory Updated (not yet, actually).')
+}
+
+customerPrompt();
